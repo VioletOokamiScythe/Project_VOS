a7610ef779d57c8829bb6b35f9c7dd9dd4001323
VioletOokamiScythe <VioletOokamiScythe@gmail.com>
2020-06-05 11:09:15 +0900

    TEST

---
+++ Client_S_Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Client_S_Main {
+    public static void main(String[] args) {
+
+    }
+}
