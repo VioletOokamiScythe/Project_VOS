7d72901a8130e2ad06ae16fefb1e388ef3443cd6
VioletOokamiScythe <VioletOokamiScythe@gmail.com>
2020-06-06 18:47:06 +0900

    add base of Clinet_S_Main (Not completed)

--- Client_S_Main.java
+++ Client_S_Main.java
@@ -3,8 +3,24 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class Client_S_Main {
+public class Client_S_Main extends JFrame{
+
+    JFrame S = new JFrame();
+
+    Client_S_Main()
+    {
+        // 기본설정
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("VOS - Client S MAIN");
+        setSize(1800, 900);
+        setVisible(true);
+
+        //왼쪽 패널 작업
+        JPanel westPanel = new JPanel(new FlowLayout());
+    }
+
     public static void main(String[] args) {
+        Client_S_Main CSM = new Client_S_Main();
         
     }
 }
