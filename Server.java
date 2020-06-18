5d9c5a1feecd4aff42090c010ec69e7c70e52a79
KCHMG <64487883+KCHMG@users.noreply.github.com>
2020-06-18 22:34:24 +0900

    로그인, 회원가입을 위한 서버작업 완료

--- Server.java
+++ Server.java
@@ -1,12 +1,69 @@
 import java.sql.*;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Server {
+    ServerSocket ss = null;
+    ArrayList<ConnectedClient> clients = new ArrayList<ConnectedClient>();
+    
     public static void main(String[] args) {
-        new DB();
-        DB.student("1", "1234", "1234", "김의종", "컴퓨터공학부");
+        Server server = new Server();
+        try {
+            server.ss = new ServerSocket(5656);
+            System.out.println("Server Socket is created");
+            while(true) {
+                Socket socket = server.ss.accept(); //소켓 생성
+                ConnectedClient cc = new ConnectedClient(socket);
+                server.clients.add(cc);
+                cc.start();
+            }
+        } catch(Exception e) {
+
+        }
+        
     }
         
 }
 
+class ConnectedClient extends Thread {
+    Socket socket;
+    OutputStream outStream;
+    DataOutputStream dataOutStream;
+    InputStream inStream;
+    DataInputStream dataInStream;
+    
+    String role;
+    String ID;
+    String PW;
+    String Name;
+    String Major;
+    DB DB = new DB();
+
+    ConnectedClient(Socket _s) {
+        this.socket = _s;
+    }
+
+    public void run() {
+        try {
+            System.out.println(this.socket.toString() + "에서 연결되었습니다.");
+            outStream = this.socket.getOutputStream();
+            dataOutStream = new DataOutputStream(outStream);
+            inStream = this.socket.getInputStream();
+            dataInStream = new DataInputStream(inStream);
+            while(true) {
+                String User = dataInStream.readUTF();
+                StringTokenizer st = new StringTokenizer(User, "/");
+                role = st.nextToken();
+                ID = st.nextToken();
+                PW = st.nextToken();
+                Name = st.nextToken();
+                Major = st.nextToken();
+                DB.student(role, ID, PW, Name, Major);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
