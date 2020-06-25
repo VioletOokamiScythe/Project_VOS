fccfd1642bb980bea4a16099fb4b38b7bd81fcbe
VioletOokamiScythe <VioletOokamiScythe@gmail.com>
2020-06-25 19:51:33 +0900

    추가 정리

--- Client_S_INROOM.java
+++ Client_S_INROOM.java
@@ -37,7 +37,6 @@ public class Client_S_INROOM extends JFrame {
         SouthPanel.add(EXAM_EXIT);
 
         EXAM_EXIT.addActionListener(SIRA);
-        getContentPane().addMouseListener(SIMK);
 
         // 기본설정
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -47,24 +46,6 @@ public class Client_S_INROOM extends JFrame {
         setUndecorated(true);
         setVisible(true);
 
-        // 이미지 받아올 소켓 생성
-        ServerSocket SS = null;
-        Socket S = null;
-
-        try {
-
-            SS = new ServerSocket(5656);
-            S = SS.accept();
-            System.out.println("클라이언트가 연결되었습니다 " + S);
-            BufferedInputStream BIS = new BufferedInputStream(S.getInputStream());
-
-            while (true) {
-                CenterPanel.getGraphics().drawImage(ImageIO.read(ImageIO.createImageInputStream(BIS)), 0, 0,
-                        CenterPanel.getWidth() / 2, CenterPanel.getHeight(), CenterPanel);
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
     }
 
     class S_InRoom_Action implements ActionListener {
@@ -79,71 +60,9 @@ public class Client_S_INROOM extends JFrame {
         }
     }
 
-    class S_InRoom_MK implements MouseListener, KeyListener {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-            // TODO Auto-generated method stub
-            try {
-                Robot robot=new Robot();
-                robot.mouseMove(EXAM_EXIT.getX(), EXAM_EXIT.getY());
-            } catch (Exception e2) {
-                //TODO: handle exception
-            }
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-            // TODO Auto-generated method stub
-            try {
-                Robot robot = new Robot();
-                robot.mouseMove(500, 600);
-            } catch (Exception e2) {
-                // TODO: handle exception
-            }
-
-        }
-
-        @Override
-        public void keyTyped(KeyEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-    }
+ 
 
     S_InRoom_Action SIRA = new S_InRoom_Action();
-    S_InRoom_MK SIMK = new S_InRoom_MK();
 
     public static void main(String[] args) {
         Client_S_INROOM CSI = new Client_S_INROOM();
